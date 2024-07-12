e444adc5238622a9b5b1131ff94d38172a352e7c
Ankit Mishra <anki.mishra@gmail.com>
2024-07-12 12:05:02 +0530

    Update yt_mp4_download.py

--- yt_mp4_download.py
+++ yt_mp4_download.py
@@ -1,27 +1,36 @@
-from pytube import YouTube 
+from pytube import YouTube
+from pytube.cli import on_progress
 
-# where to save 
-SAVE_PATH = "/home/chintusharma/Downloads"
+# where to save
+SAVE_PATH = "C:\\Users\\Ankit Mishra\\PycharmProjects\\VideoIntelligence"
 
-# link of the video to be downloaded 
-link = "https://youtube.com/shorts/akL53YfPTfI?si=ju1ohfkBdFSInLNj"
+# link of the video to be downloaded
+link = "https://www.youtube.com/watch?v=7m4zQpf3Ouo"
 
-try: 
-    # object creation using YouTube 
-    yt = YouTube(link) 
-except: 
-    #to handle exception 
-    print("Connection Error") 
+"""
+    Running the code inside the try/except block
+"""
 
-# Get all streams and filter for mp4 files
-mp4_streams = yt.streams.filter(file_extension='mp4').all()
 
-# get the video with the highest resolution
-d_video = mp4_streams[-1]
+# this is the handler/function for downloading the video
+# the function takes the video url as an argument
+def video_downloader(video_url):
+    # passing the video url and progress_callback to the YouTube object
+    my_video = YouTube(video_url, on_progress_callback=on_progress)
+    # downloading the video in high resolution
+    my_video.streams.get_highest_resolution().download()
+    # return the video title
+    return my_video.title
 
-try: 
-    # downloading the video 
-    d_video.download(output_path=SAVE_PATH)
-    print('Video downloaded successfully!')
-except: 
-    print("Some Error!")
+
+try:
+    # getting the url from the user
+    youtube_link = input('Enter the YouTube link:')
+    print(f'Downloading your Video, please wait.......')
+    # passing the url to the function
+    video = video_downloader(youtube_link)
+    # printing the video title
+    print(f'"{video}" downloaded successfully!!')
+
+except:
+    print(f'Failed to download video')
